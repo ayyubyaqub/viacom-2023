e4524399fe75eed7c0f152f8bb42ed0a934a94ed
ayyubyaqub <ayyubkhan4341@gmail.com>
2022-12-31 17:00:44 +0530

    contact us temp

--- home/views.py
+++ home/views.py
@@ -72,7 +72,7 @@ def index(request):
 <article>
     <h1>We&rsquo;ll be back soon!</h1>
     <div>
-        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="https://viacomindia.com/contactus">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
+        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="contactus">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
         <p>&mdash; The Team</p>
     </div>
 </article>''')
